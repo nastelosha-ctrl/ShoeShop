6f3afe6c2168192242f28d29c62f2ea5894b18c1
Skvortsova <nastelosha@gmail.com>
2026-03-02 14:45:51 +0300

    Добавили цвета для темы

--- ShoeShop/app/src/main/java/com/example/shoeshop/ui/theme/Color.kt
+++ ShoeShop/app/src/main/java/com/example/shoeshop/ui/theme/Color.kt
@@ -2,10 +2,23 @@ package com.example.shoeshop.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Red = Color(0xFFF87265)
+val Accent = Color(0xFF48B2E7)
+val Disable = Color(0xFF2B6B8B)
+val SubTextLight = Color(0xFFD8D8D8)
+val Background = Color(0xFFF7F7F9)
+val Block = Color(0xFFFFFFFF)
+val Hint = Color(0xFF6A6A6A)
+val Text = Color(0xFF2B2B2B)
+val SubtextDark = Color(0xFF707B81)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Для темной темы
+val DarkBackground = Color(0xFF121212)
+val DarkBlock = Color(0xFF1E1E1E)
+val DarkText = Color(0xFFE1E1E1)
+val DarkHint = Color(0xFFAAAAAA)
+
+// Дополнительные цвета если нужно
+val Success = Color(0xFF4CAF50)
+val Warning = Color(0xFFFF9800)
+val Info = Color(0xFF2196F3)
