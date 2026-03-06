626035558e53ee20adc9db81c46df508256cdfdb
Skvortsova <nastelosha@gmail.com>
2026-03-06 11:43:19 +0300

    Готовые 3 дня

--- ShoeShop/app/src/main/java/com/example/shoeshop/data/RetrofitInstance.kt
+++ ShoeShop/app/src/main/java/com/example/shoeshop/data/RetrofitInstance.kt
@@ -7,22 +7,22 @@ import java.net.InetSocketAddress
 import java.net.Proxy
 
 object RetrofitInstance {
-    const val SUBABASE_URL = "https://xnweiojtzqjnsdwrrrfi.supabase.co/"
+    const val SUBABASE_URL = "https://ubrorvghkpghapgcunvr.supabase.co/"
 
-        //private val proxy= Proxy(Proxy.Type.HTTP, InetSocketAddress("10.207.106.59",3128))
+    private val proxy= Proxy(Proxy.Type.HTTP, InetSocketAddress("10.207.106.59",3128))
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
     }
    private val client = OkHttpClient.Builder()
-//        .proxy(proxy)
-//        .addInterceptor(loggingInterceptor)
-//        .build()
+        .proxy(proxy)
+        .addInterceptor(loggingInterceptor)
+        .build()
 
 
     private  val retrofit = Retrofit.Builder()
         .baseUrl(SUBABASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
-       // .client(client)
+        .client(client)
         .build()
     val userManagementService = retrofit.create(UserManagementService::class.java)
 }
